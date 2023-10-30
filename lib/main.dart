1122b73ec765b920d4b0c0632910c1561add7aa4
Arturo Ramirez - LGL <demonoid@outlook.jp>
2023-10-30 00:53:22 -0600

    Stable design 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -21,75 +21,82 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int _selectedIndex = 0;
 
-  final List<Widget> _pages = [
-    Center(
-      child: Text('Pantalla de Inicio'),
-    ),
-    Center(
-      child: Text('Pantalla de Capturar Reporte'),
-    ),
-    Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            Icons.description,
-            // Cambia a un icono relacionado a informes o documentación
-            size: 48,
-          ),
-          SizedBox(height: 16),
-          Text('Pantalla de Ver Reportes'),
-        ],
-      ),
-    ),
-    Center(
-      child: Text('Pantalla de Usuario'),
-    ),
-  ];
-
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
+  final List<Widget> _pages = [
+    _buildPage('Pantalla de Inicio'),
+    _buildPage('Pantalla de Capturar Reporte'),
+    _buildPage('Pantalla de Ver Reportes'),
+    _buildPage('Pantalla de Usuario'),
+  ];
+
+  static Widget _buildPage(String text, {IconData? icon}) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          if (icon != null)
+            Icon(
+              icon,
+              size: 48,
+              color: Colors.black,
+            ),
+          SizedBox(height: 16),
+          Text(
+            text,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Generador de reportes'),
+        title: Text(
+          'Generador de Reportes',
+          style: TextStyle(
+            color: Colors.black, // Color del título
+            fontSize: 24, // Tamaño del título
+            fontWeight: FontWeight.bold, // Negrita
+          ),
+        ),
       ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Inicio',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add),
-            label: 'Capturar Reporte',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.description),
-            label: 'Ver Reportes',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Usuario',
-          ),
+          _buildBottomNavItem(Icons.home, 'Inicio'),
+          _buildBottomNavItem(Icons.add, 'Capturar Reporte'),
+          _buildBottomNavItem(Icons.description, 'Ver Reportes'),
+          _buildBottomNavItem(Icons.account_circle, 'Usuario'),
         ],
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
         backgroundColor: Colors.grey[200],
-        // Color de fondo de la barra de navegación (gris ligero)
         unselectedItemColor: Colors.black,
-        // Color de íconos no seleccionados (negro)
         selectedItemColor: Colors.black,
-        // Color de íconos seleccionados (negro)
-        selectedLabelStyle: TextStyle(
-            color: Colors.white), // Color del texto seleccionado (blanco)
+        selectedLabelStyle: TextStyle(color: Colors.white),
       ),
+    );
+  }
+
+  static BottomNavigationBarItem _buildBottomNavItem(
+      IconData icon, String label) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        icon,
+        color: Colors.black,
+      ),
+      label: label,
     );
   }
 }
